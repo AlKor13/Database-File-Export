dda10d0307fab484d3cd95aafbd150c9b2615877
George <jora.grintsevich@gmail.com>
2022-09-09 17:31:59 +0500

    :lipstick: Added controls to MainPanel

--- DatabaseFileExport/MainForm.Designer.cs
+++ DatabaseFileExport/MainForm.Designer.cs
@@ -37,17 +37,19 @@
             this.SelectFileButton = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.TableColumn2ComboBox = new System.Windows.Forms.ComboBox();
+            this.FilterTableComboBox = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.TableColumnComboBox = new System.Windows.Forms.ComboBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.ColumnToUpdateComboBox = new System.Windows.Forms.ComboBox();
+            this.FilterTextBox = new System.Windows.Forms.TextBox();
+            this.SQLQueryPanel = new System.Windows.Forms.Panel();
             this.DBTableNameTextBox = new System.Windows.Forms.TextBox();
             this.CheckConnectionStringButton = new System.Windows.Forms.Button();
             this.label6 = new System.Windows.Forms.Label();
             this.SelectDataBaseTableComboBox = new System.Windows.Forms.ComboBox();
-            this.panel1.SuspendLayout();
+            this.DBTablePanel = new System.Windows.Forms.Panel();
+            this.SQLQueryPanel.SuspendLayout();
+            this.DBTablePanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // ConnectionStringTextBox
@@ -61,7 +63,7 @@
             // 
             this.LogoLabel.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.LogoLabel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.LogoLabel.Location = new System.Drawing.Point(9, 25);
+            this.LogoLabel.Location = new System.Drawing.Point(9, 9);
             this.LogoLabel.Margin = new System.Windows.Forms.Padding(0);
             this.LogoLabel.Name = "LogoLabel";
             this.LogoLabel.Size = new System.Drawing.Size(278, 34);
@@ -86,6 +88,7 @@
             this.InsertButton.TabIndex = 5;
             this.InsertButton.Text = "Экспорт";
             this.InsertButton.UseVisualStyleBackColor = true;
+            this.InsertButton.Visible = false;
             this.InsertButton.Click += new System.EventHandler(this.InsertButton_Click);
             // 
             // FileNameLable
@@ -97,9 +100,10 @@
             // 
             // label3
             // 
-            this.label3.Location = new System.Drawing.Point(266, 5);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(17)))), ((int)(((byte)(255)))));
+            this.label3.Location = new System.Drawing.Point(268, 6);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(16, 21);
+            this.label3.Size = new System.Drawing.Size(16, 16);
             this.label3.TabIndex = 12;
             this.label3.Text = "=";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -116,80 +120,84 @@
             // 
             // label4
             // 
-            this.label4.Location = new System.Drawing.Point(386, 6);
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(17)))), ((int)(((byte)(255)))));
+            this.label4.Location = new System.Drawing.Point(386, 8);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(48, 21);
+            this.label4.Size = new System.Drawing.Size(48, 17);
             this.label4.TabIndex = 13;
             this.label4.Text = "WHERE";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(3, 7);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(17)))), ((int)(((byte)(255)))));
+            this.label1.Location = new System.Drawing.Point(3, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 21);
+            this.label1.Size = new System.Drawing.Size(55, 15);
             this.label1.TabIndex = 8;
             this.label1.Text = "UPDATE";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // TableColumn2ComboBox
+            // FilterTableComboBox
             // 
-            this.TableColumn2ComboBox.FormattingEnabled = true;
-            this.TableColumn2ComboBox.Location = new System.Drawing.Point(440, 6);
-            this.TableColumn2ComboBox.Name = "TableColumn2ComboBox";
-            this.TableColumn2ComboBox.Size = new System.Drawing.Size(82, 21);
-            this.TableColumn2ComboBox.TabIndex = 14;
+            this.FilterTableComboBox.FormattingEnabled = true;
+            this.FilterTableComboBox.Location = new System.Drawing.Point(440, 6);
+            this.FilterTableComboBox.Name = "FilterTableComboBox";
+            this.FilterTableComboBox.Size = new System.Drawing.Size(82, 21);
+            this.FilterTableComboBox.TabIndex = 14;
             // 
             // label2
             // 
-            this.label2.Location = new System.Drawing.Point(144, 4);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(17)))), ((int)(((byte)(255)))));
+            this.label2.Location = new System.Drawing.Point(144, 9);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(28, 21);
+            this.label2.Size = new System.Drawing.Size(28, 15);
             this.label2.TabIndex = 9;
             this.label2.Text = "SET";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label5
             // 
-            this.label5.Location = new System.Drawing.Point(528, 6);
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(17)))), ((int)(((byte)(255)))));
+            this.label5.Location = new System.Drawing.Point(528, 5);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(16, 21);
+            this.label5.Size = new System.Drawing.Size(16, 17);
             this.label5.TabIndex = 15;
             this.label5.Text = "=";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // TableColumnComboBox
+            // ColumnToUpdateComboBox
             // 
-            this.TableColumnComboBox.FormattingEnabled = true;
-            this.TableColumnComboBox.Location = new System.Drawing.Point(178, 5);
-            this.TableColumnComboBox.Name = "TableColumnComboBox";
-            this.TableColumnComboBox.Size = new System.Drawing.Size(82, 21);
-            this.TableColumnComboBox.TabIndex = 7;
+            this.ColumnToUpdateComboBox.FormattingEnabled = true;
+            this.ColumnToUpdateComboBox.Location = new System.Drawing.Point(178, 6);
+            this.ColumnToUpdateComboBox.Name = "ColumnToUpdateComboBox";
+            this.ColumnToUpdateComboBox.Size = new System.Drawing.Size(82, 21);
+            this.ColumnToUpdateComboBox.TabIndex = 7;
             // 
-            // textBox1
+            // FilterTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(550, 7);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(124, 20);
-            this.textBox1.TabIndex = 16;
+            this.FilterTextBox.Location = new System.Drawing.Point(550, 6);
+            this.FilterTextBox.Name = "FilterTextBox";
+            this.FilterTextBox.Size = new System.Drawing.Size(124, 20);
+            this.FilterTextBox.TabIndex = 16;
             // 
-            // panel1
+            // SQLQueryPanel
             // 
-            this.panel1.Controls.Add(this.DBTableNameTextBox);
-            this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Controls.Add(this.TableColumnComboBox);
-            this.panel1.Controls.Add(this.label5);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.TableColumn2ComboBox);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.SelectFileButton);
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Location = new System.Drawing.Point(15, 195);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(678, 33);
-            this.panel1.TabIndex = 18;
-            this.panel1.Visible = false;
+            this.SQLQueryPanel.Controls.Add(this.DBTableNameTextBox);
+            this.SQLQueryPanel.Controls.Add(this.FilterTextBox);
+            this.SQLQueryPanel.Controls.Add(this.ColumnToUpdateComboBox);
+            this.SQLQueryPanel.Controls.Add(this.label5);
+            this.SQLQueryPanel.Controls.Add(this.label2);
+            this.SQLQueryPanel.Controls.Add(this.FilterTableComboBox);
+            this.SQLQueryPanel.Controls.Add(this.label1);
+            this.SQLQueryPanel.Controls.Add(this.label4);
+            this.SQLQueryPanel.Controls.Add(this.SelectFileButton);
+            this.SQLQueryPanel.Controls.Add(this.label3);
+            this.SQLQueryPanel.Location = new System.Drawing.Point(15, 195);
+            this.SQLQueryPanel.Name = "SQLQueryPanel";
+            this.SQLQueryPanel.Size = new System.Drawing.Size(678, 33);
+            this.SQLQueryPanel.TabIndex = 18;
+            this.SQLQueryPanel.Visible = false;
             // 
             // DBTableNameTextBox
             // 
@@ -212,20 +220,30 @@
             // label6
             // 
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label6.Location = new System.Drawing.Point(12, 137);
+            this.label6.Location = new System.Drawing.Point(0, 6);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(242, 15);
+            this.label6.Size = new System.Drawing.Size(228, 15);
             this.label6.TabIndex = 20;
             this.label6.Text = "Выберете таблицу базы данных";
             // 
             // SelectDataBaseTableComboBox
             // 
             this.SelectDataBaseTableComboBox.FormattingEnabled = true;
-            this.SelectDataBaseTableComboBox.Location = new System.Drawing.Point(15, 155);
+            this.SelectDataBaseTableComboBox.Location = new System.Drawing.Point(0, 24);
             this.SelectDataBaseTableComboBox.Name = "SelectDataBaseTableComboBox";
-            this.SelectDataBaseTableComboBox.Size = new System.Drawing.Size(200, 21);
+            this.SelectDataBaseTableComboBox.Size = new System.Drawing.Size(203, 21);
             this.SelectDataBaseTableComboBox.TabIndex = 21;
             this.SelectDataBaseTableComboBox.SelectionChangeCommitted += new System.EventHandler(this.SelectDataBaseTableComboBox_SelectionChangeCommitted);
+            // 
+            // DBTablePanel
+            // 
+            this.DBTablePanel.Controls.Add(this.label6);
+            this.DBTablePanel.Controls.Add(this.SelectDataBaseTableComboBox);
+            this.DBTablePanel.Location = new System.Drawing.Point(15, 128);
+            this.DBTablePanel.Name = "DBTablePanel";
+            this.DBTablePanel.Size = new System.Drawing.Size(231, 48);
+            this.DBTablePanel.TabIndex = 22;
+            this.DBTablePanel.Visible = false;
             // 
             // Form1
             // 
@@ -233,10 +251,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(727, 302);
-            this.Controls.Add(this.SelectDataBaseTableComboBox);
-            this.Controls.Add(this.label6);
+            this.Controls.Add(this.DBTablePanel);
             this.Controls.Add(this.CheckConnectionStringButton);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.SQLQueryPanel);
             this.Controls.Add(this.FileNameLable);
             this.Controls.Add(this.InsertButton);
             this.Controls.Add(this.ConnectionStringLabel);
@@ -244,12 +261,14 @@
             this.Controls.Add(this.ConnectionStringTextBox);
             this.Location = new System.Drawing.Point(15, 15);
             this.Name = "Form1";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.SQLQueryPanel.ResumeLayout(false);
+            this.SQLQueryPanel.PerformLayout();
+            this.DBTablePanel.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
+
+        private System.Windows.Forms.Panel DBTablePanel;
 
         private System.Windows.Forms.TextBox DBTableNameTextBox;
 
@@ -262,12 +281,12 @@
         private System.Windows.Forms.Button SelectFileButton;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox TableColumn2ComboBox;
+        private System.Windows.Forms.ComboBox FilterTableComboBox;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.ComboBox TableColumnComboBox;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox ColumnToUpdateComboBox;
+        private System.Windows.Forms.TextBox FilterTextBox;
+        private System.Windows.Forms.Panel SQLQueryPanel;
         private System.Windows.Forms.Button CheckConnectionStringButton;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.ComboBox SelectDataBaseTableComboBox;
